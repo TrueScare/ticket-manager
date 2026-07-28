2306816dfdc4c9452953938582addf294cb1c786
Liam Mefus <liam.mefus@outlook.de>
2026-07-28 21:50:55 +0200

    finish up routes pre db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,17 @@
-import express, {Request, Response} from 'express';
+import express, {Response} from 'express';
 import dotenv from 'dotenv';
+import taskRoutes from "./routes/task.routes.js";
 
 dotenv.config();
 
 const server = express();
 const port = process.env.PORT || 3001;
 
-server.get("/", (req: Request, res: Response) => {
+server.get("/", (res: Response) => {
     res.send("Hello World!");
 });
+
+server.use("/tasks", taskRoutes);
 
 server.listen(port, () => {
     console.log("Server is running on port", port);
--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -16,44 +16,43 @@ const list: Task[] = [
     }
 ];
 router.get('/', (req: Request, res: Response) => {
-    return res.json({
-        data: list
-    })
+    return res.json({details: list})
 });
 router.get('/:id', (req: Request, res: Response) => {
     const task = list.find(task => task.id === parseInt(<string>req.params.id));
     if (!task) {
         return res.status(404).json({details: `Task with id not found. Given id: ${req.params.id}`});
     }
-    return res.status(200).json({task});
+    return res.status(200).json({details: task});
 });
 router.post('/', (req: Request, res: Response) => {
     const task: Task = req.body;
     if (!validateTask(task)) {
-        res.status(422).json({details: `Invalid format for task: ${task}`});
+        return res.status(422).json({details: `Invalid format for task: ${task}`});
     }
     list.push(task);
-    return res.status(200).json({details: list});
+    return res.status(201).json({details: task});
 });
 router.put('/:id', (req: Request, res: Response) => {
-    const task= list.find(({id}) => id === parseInt(<string>req.params.id));
+    const task = list.find(({id}) => id === parseInt(<string>req.params.id));
     if (!task) {
         return res.status(404).json({details: `Kein Task vorhanden für id: ${req.params.id}`});
     }
     const updatedTask: Task = req.body;
-    if(!validateTask(updatedTask)) {
-        res.status(422).json({details: `Invalid format for task: ${task}`});
+    if (!validateTask(updatedTask)) {
+        return res.status(422).json({details: `Invalid format for task: ${task}`});
     }
     list[list.indexOf(task)] = updatedTask;
-    return res.status(200).json({details: list});
+    return res.status(200).json({details: task});
 });
 router.delete('/:id', (req: Request, res: Response) => {
     const task: Task | undefined = list.find(({id}) => id === parseInt(<string>req.params.id));
     if (!task) {
-        res.status(404).json({details: `Kein Task vorhanden für id: ${req.params.id}`});
+        return res.status(404).json({details: `Kein Task vorhanden für id: ${req.params.id}`});
     }
-    list.splice(list.indexOf(<Task>task), 1);
-    return res.status(200).json({details: list});
+
+    list.splice(list.indexOf(task), 1);
+    return res.status(200).json({details: task});
 });
 
 export default router;
