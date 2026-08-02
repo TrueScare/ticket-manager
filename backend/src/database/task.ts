fca431ffb72247c2562dff788a04cd7a9c8aaaf7
Liam Mefus <liam.mefus@outlook.de>
2026-08-02 11:02:51 +0200

    fix sql-injection problems and finalize api

--- backend/src/database/task.ts
+++ backend/src/database/task.ts
@@ -6,28 +6,19 @@ async function getAllTasks(): Promise<Task[]> {
 }
 
 async function getTaskById(id: number): Promise<Task> {
-    return await db.one(`SELECT *
-                         FROM task
-                         WHERE id = ${id}`);
+    return await db.one('SELECT * FROM task WHERE id = $1', [id]);
 }
 
 async function addTask(task: Task): Promise<Task> {
-    return await db.one(`INSERT INTO task(title, isDone) 
-                         values ('${task.title}', ${task.isDone})
-                         RETURNING *`);
+    return await db.one('INSERT INTO task(title, isDone) values ($1, $2) RETURNING *', [task.title, task.isDone]);
 }
 
 async function updateTask(task: Task): Promise<Task> {
-    return await db.one(`UPDATE task
-                         set title  = '${task.title}',
-                             isDone = ${task.isDone}`);
+    return await db.one('UPDATE task set title  = $1, isDone = $2  where id = $3 RETURNING *', [task.title, task.isDone, task.id]);
 }
 
 async function deleteTask(task: Task): Promise<Task> {
-    return await db.one(`DELETE
-                         FROM task
-                         WHERE id = ${task.id}
-                         RETURNING *`);
+    return await db.one('DELETE FROM task WHERE id = $1 RETURNING *', [task.id]);
 }
 
 export {getAllTasks, addTask, updateTask, deleteTask, getTaskById};
--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -6,47 +6,47 @@ const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
     const tasks = await getAllTasks();
-    return res.json({tasks});
+    return res.json({details: tasks});
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
     const task = await getTaskById(parseInt(<string>req.params.id));
     if (!task) {
-        return res.status(404).json({message: `Task with id not found. Given id: ${req.params.id}`});
+        return res.status(404).json({details: `Task with id not found. Given id: ${req.params.id}`});
     }
-    return res.status(200).json({task});
+    return res.status(200).json({details: task});
 });
 
 router.post('/', async (req: Request, res: Response) => {
     const task: Task = req.body;
-    if (!task || !validateTask(task)) {
-        return res.status(422).json({message: `Invalid format for task: ${task}, ${req.body}`});
+    if (!validateTask(task)) {
+        return res.status(422).json({details: `Invalid format for task: ${task}, ${req.body}`});
     }
     const db_task = await addTask(task);
-    return res.status(201).json({task: db_task});
+    return res.status(201).json({details: db_task});
 });
 
 router.put('/:id', async (req: Request, res: Response) => {
     const task = await getTaskById(parseInt(<string>req.params.id));
     if (!task) {
-        return res.status(404).json({message: `Kein Task vorhanden für id: ${req.params.id}`});
+        return res.status(404).json({details: `Kein Task vorhanden für id: ${req.params.id}`});
     }
     const updatedTask: Task = req.body;
     if (!validateTask(updatedTask)) {
-        return res.status(422).json({message: `Invalid format for task: ${task}`});
+        return res.status(422).json({details: `Invalid format for task: ${task}`});
     }
-    const db_task = await updateTask(task);
-    return res.status(200).json({task: db_task});
+    const db_task = await updateTask(updatedTask);
+    return res.status(200).json({details: db_task});
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
     const task: Task | undefined = await getTaskById(parseInt(<string>req.params.id));
     if (!task) {
-        return res.status(404).json({message: `Kein Task vorhanden für id: ${req.params.id}`});
+        return res.status(404).json({details: `Kein Task vorhanden für id: ${req.params.id}`});
     }
 
     const db_task = await deleteTask(task);
-    return res.status(200).json({task: db_task});
+    return res.status(200).json({details: db_task});
 });
 
 export default router;
